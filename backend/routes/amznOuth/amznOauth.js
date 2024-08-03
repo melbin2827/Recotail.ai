25b02baba7cebdc30f6c7fd89942eccad9e29859
harsh@recotail.ai <harsh>
2024-08-03 10:34:10 +0530

    amznoauth

--- backend/routes/amznOuth/amznOauth.js
+++ backend/routes/amznOuth/amznOauth.js
@@ -4,16 +4,18 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const redirectUrl = "http://localhost:4000/auth/amznOauth/redirect";
-const appID = process.env.APPLICATION_ID;
+const appID = "amzn1.sp.solution.591b8b7e-4d05-44ba-9259-8e4fb0a4669d";
 
 router.get('/', (req, res) => {
-  const oauthUrl = `https://sellercentral.amazon.com/apps/authorize/consent?application_id=${appID}&version=beta&redirect_uri=${redirectUrl}`;
+  console.log('Oauth');
+  const oauthUrl = `https://sellercentral.amazon.com/apps/authorize/consent?application_id=${appID}&state=1234509876&version=beta`;
   res.redirect(oauthUrl); // Redirect to Amazon's OAuth page
 });
 
-router.get('/redirect', (req, res) => {
-  console.log(req.query);
-  res.send("Amazon OAuth process completed");
+router.post('/callback', (req, res) => {
+  console.log('amzn auth callbacl', req.query);
+  const {spapi_oauth_code,state,selling_partner_id} = req.query;
+  const {refresh_token, access_token} = await axios.post(`https://api.amazon.com/auth/o2/token?grant_type=authorization_code&code=${spapi_oauth_code}&redirect_uri=recotail.ai&client_id=${}&client_secret=${}`)
 });
 
 export default router;
---
+++ backend/temp.js
@@ -0,0 +1,3 @@
+import crypto from "crypto";
+
+console.log(crypto.randomUUID()); // Example output: '9b1c79f6b24f87e1'
