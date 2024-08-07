f39f4d9dd28fd1747b5160d00adf3cef1327aeac
harsh@recotail.ai <harsh>
2024-08-07 11:05:17 +0530

    a

--- backend/routes/amznOuth/amznOauth.js
+++ backend/routes/amznOuth/amznOauth.js
@@ -9,7 +9,7 @@ const redirectUrl = process.env.AMZN_REDIRECT_URL;
 const appID = process.env.APPLICATION_ID;
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const stateToken = crypto.randomUUID();
+let stateToken = crypto.randomUUID();
 
 router.get('/', (req, res) => {
   console.log('Oauth');
@@ -18,50 +18,31 @@ router.get('/', (req, res) => {
 });
 
 router.post('/callback', async(req, res) => {
-  console.log('amzn auth callbacl', req.query);
   try{
-    const {spapiOauthCode,state,sellingPartnerId} = req.query;
+    const {spapi_oauth_code,state,selling_partner_id} = req.query;
     if(state != stateToken){
       console.error("state token does not match");
-      return res.status(400).send("state token does not match");
+      return res.status(400).send("State token does not match");
     }
-    const {refreshToken, accessToken} = await axios.post(`https://api.amazon.com/auth/o2/token?grant_type=authorization_code&code=${spapiOauthCode}&redirect_uri=${redirectUrl}&client_id=${clientId}&client_secret=${clientSecret}`);
-    const headers = {
-      'Authorization': `Bearer ${accessToken}`,
-      'x-amz-access-token': accessToken,
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    };
-    const shopInfo = await axios.get(`sellingpartnerapi-na.amazon.com/sellers/v1/marketplaceParticipations`,{headers});
-    console.log(shopInfo.data);
-    res.send(shopInfo.data);
+
+    // get refresh token using spapi_oauth_code
+    const {refreshToken, accessToken} = await axios.post(`https://api.amazon.com/auth/o2/token?grant_type=authorization_code&code=${spapi_oauth_code}&client_id=${clientId}&client_secret=${clientSecret}`);
+    if(!refreshToken){
+      console.error("fail to get access token");
+      return res.status(400).send("Fail to get access token");
+    }
+    
+    res.json({
+      oauth_code: spapi_oauth_code,
+      amazonselling_partner_id: selling_partner_id,
+      refresh_token: refreshToken
+    }).status(200);
   }
   catch(err){
     console.log(err);
+    res.status(500).send("Internal server error");
   }
 });
 
 export default router;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-router;
